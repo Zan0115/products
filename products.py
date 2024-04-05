165860e44f7f7949be7411ef6a7a952f4ef1c004
Zan Lin <oaobj4@gmail.com>
2024-04-05 17:46:43 +0800

    add read file

--- products.py
+++ products.py
@@ -1,4 +1,14 @@
+# 打開文件檔，將文件檔內資料轉成list
 products = []
+with open('product.csv', 'r', encoding = 'utf-8') as f:
+    for line in f:
+        if '商品,價格' in line: # 跳過第一列資料
+            continue # 跳到迴圈下一round
+        name, price = line.strip().split(',') # 用split(',')來用逗點做分割，分割完的結果是list；用strip來除掉換行符號
+        products.append([name, price])
+print(products)
+
+# 輸入商品名稱、價格，並存到文件檔
 while True:
     name = input("請輸入商品名稱 : ")
     if name == 'q':
@@ -7,9 +17,11 @@ while True:
     products.append([name, price]) # 將list append進list內
 print(products)
 
+# 印出所有購買紀錄
 for p in products:
     print(p[0], "的價格是 :", p[1])
 
+# 寫入檔案
 with open('product.csv', 'w', encoding = 'utf-8') as f: # 開啟(沒有的話就建立)product.csv檔案，並設為寫入模式
     f.write('商品,價格\n')
     for p in products:
